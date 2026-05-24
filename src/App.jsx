c02624350de206ab667a42edf1c51e1abed85173
lellisjr88 <elsj1988@gmail.com>
2026-05-23 23:22:48 -0300

    Estrutura inicial portfolio com React e Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Olá, eu sou Lellis Júnior</h1>
+
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+   return( <>
+        <h1>Lellis Júnior</h1>
+        <p>Front-End Developer</p>
+    </>)
+}
+
+export default Header
