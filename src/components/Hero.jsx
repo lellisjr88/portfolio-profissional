d9e54e538f552c79c532dd4a9babd438e7234471
Lellis Júnior <92763629+lellisjr88@users.noreply.github.com>
2026-06-12 15:58:50 +0000

    Criada estrutura inicial da seção hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
+import './Hero.css'
+
 function Hero() {
     return (
-        <section className="hero">
-           <p>Componente Hero.</p>
+        <section id="hero">
+            <div className="hero-content">
+                <p>Olá, eu sou </p>
+                <h1>Lellis Júnior</h1>
+                <h2>Desenvolvedor Front-End</h2>
+                <p className='hero-description'>Crio interfaces web modernas, responsivas e funcionais, unindo tecnologia,
+                    organização e criatividade para entregar soluções de alta qualidade.
+                </p>
+                <div className="hero-actions">
+                    <a href="#projetos">Ver projetos</a>
+                    <a href="#contato">Entrar em contato</a>
+                </div>
+            </div>
         </section>
     )
 }
